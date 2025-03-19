57512cdd79be72cfec642432ee3853bc7002ff51
Михаил Гилодо <mgilodo@gmail.com>
2025-03-19 17:39:39 +0200

    + решения задач про квартиру и фигуры

--- js/functions.js
+++ js/functions.js
@@ -1,74 +1,42 @@
-// ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ДЛИНЫ СТРОКИ
-// Вариант 1 (с if)
-function checkStringLength1(string, stringMaxLength) {
-  const str1 = string.toString();
-  const stringLength = str1.length;
-  if (stringLength <= stringMaxLength) {
-    return true;
-  }
-  return false;
-}
-console.log(checkStringLength1('проверяемая строка', 20));
-console.log(checkStringLength1('проверяемая строка', 18));
-console.log(checkStringLength1('проверяемая строка', 10));
-console.log('--ПРОВЕРКА ДЛИНЫ СТРОКИ ЗАВЕРШЕНА--');
-
-// Вариант 2 (с тернарным оператором)
-function checkStringLength2(string, stringMaxLength) {
-  const str1 = string.toString();
-  const stringLength = str1.length;
-  return (stringLength <= stringMaxLength) ? true : false;
-}
-console.log(checkStringLength2('проверяемая строка', 20));
-console.log(checkStringLength2('проверяемая строка', 18));
-console.log(checkStringLength2('проверяемая строка', 10));
+// 1. ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ДЛИНЫ СТРОКИ
+const checkCharsetLength = (charset = '', maxLength = 1) => charset.toString().length <= maxLength;
+console.log(checkCharsetLength('проверяемая строка', 20));
+console.log(checkCharsetLength('проверяемая строка', 18));
+console.log(checkCharsetLength('проверяемая строка', 10));
 console.log('--ПРОВЕРКА ДЛИНЫ СТРОКИ ЗАВЕРШЕНА--');
 
 
-// ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ЯВЛЯВЕТСЯ ЛИ СТРОКА ПАЛИНДРОМОМ
-// Вариант 1 (строка без пробелов)
-function checkIfPalindrome1(string) {
-  let reversedString = '';
-  const strLowerCase = string.toString().toLowerCase();
-  for (let index = strLowerCase.length - 1; index >= 0; index--) {
-    reversedString += strLowerCase[index];
+// 2. ФУНКЦИЯ ДЛЯ ПРОВЕРКИ ЯВЛЯВЕТСЯ ЛИ СТРОКА ПАЛИНДРОМОМ
+function isPalindrome(charset = '') {
+  let reversedCharset = '';
+  const charsetLowerCase = charset.toString().toLowerCase().replaceAll(' ', '');
+  for (let i = charsetLowerCase.length - 1; i >= 0; i--) {
+    reversedCharset += charsetLowerCase[i];
   }
-  return (strLowerCase === reversedString) ? true : false;
+  return charsetLowerCase === reversedCharset;
 }
-console.log(checkIfPalindrome1('топот'));
-console.log(checkIfPalindrome1('ДовОд'));
-console.log(checkIfPalindrome1('Кекс'));
-console.log('--ПРОВЕРКА СТРОКИ НА ПАЛИНДРОМ ЗАВЕРШЕНА--');
-
-// Вариант 2 (строка с пробелами)
-function checkIfPalindrome2(string) {
-  let reversedString = '';
-  const strLowerCaseWithoutSpaces = string.toString().toLowerCase().replaceAll(' ', '');
-  for (let index = strLowerCaseWithoutSpaces.length - 1; index >= 0; index--) {
-    reversedString += strLowerCaseWithoutSpaces[index];
-  }
-  return (strLowerCaseWithoutSpaces === reversedString) ? true : false;
-}
-console.log(checkIfPalindrome2('Лёша на полке клопа нашёл '));
+console.log(isPalindrome('топот'));
+console.log(isPalindrome('ДовОд'));
+console.log(isPalindrome('Кекс'));
+console.log(isPalindrome('Лёша на полке клопа нашёл '));
 console.log('--ПРОВЕРКА СТРОКИ НА ПАЛИНДРОМ ЗАВЕРШЕНА--');
 
 
-// ФУНКЦИЯ ДЛЯ ИЗВЛЕЧЕНИЯ ЦИФР ИЗ СТРОКИ
+// 3. ФУНКЦИЯ ДЛЯ ИЗВЛЕЧЕНИЯ ЦИФР ИЗ СТРОКИ
 // Вариант 1 (с использованием isFinite)
-function extractNumbers1(string) {
-  let resultingString = '';
-  const str2 = string.toString();
-  for (let index = 0; index < str2.length; index++) {
-    if (isFinite(str2[index]) && str2[index] !== ' ' && str2[index] !== '-') {
-      resultingString += str2[index];
+function extractNumbers1(charset) {
+  let resultingCharset = '';
+  const readyCharset = charset.toString().replaceAll(' ', '');
+  for (let i = 0; i < readyCharset.length; i++) {
+    if (isFinite(readyCharset[i])) {
+      resultingCharset += readyCharset[i];
     }
-    continue;
   }
-  return (isFinite(resultingString) && resultingString !== '') ? Number(resultingString) : 'Цифр от 0 до 9 не обнаружено';
+  return parseInt(resultingCharset, 10);
 }
 console.log(extractNumbers1('2023 год'));
 console.log(extractNumbers1('ECMAScript 2022'));
-console.log(extractNumbers1('1 кефир, 0.5 батона'));
+console.log(extractNumbers1('1 кефир, - 0.5 батона'));
 console.log(extractNumbers1('агент 007'));
 console.log(extractNumbers1('а я томат'));
 console.log(extractNumbers1(2023));
@@ -77,16 +45,16 @@ console.log(extractNumbers1(1.5));
 console.log('--ИЗВЛЕЧЕНИЕ ЦИФР ЗАВЕРШЕНО--');
 
 // Вариант 2 (с использованием Number.isNaN)
-function extractNumbers2(string) {
-  let resultingString = '';
-  const str2 = string.toString();
-  for (let index = 0; index < str2.length; index++) {
-    if (Number.isNaN(parseInt(str2[index])) || str2[index] === ' ' || str2[index] === '-') {
-      continue;
+function extractNumbers2(charset) {
+  let resultingCharset = '';
+  const readyCharset = charset.toString().replaceAll(' ', '');
+  for (let i = 0; i < readyCharset.length; i++) {
+    if (!Number.isNaN(parseInt(readyCharset[i], 10))) {
+      resultingCharset += readyCharset[i];
     }
-    resultingString += str2[index];
+
   }
-  return (resultingString !== '') ? Number(resultingString) : 'Цифр от 0 до 9 не обнаружено';
+  return parseInt(resultingCharset, 10);
 }
 console.log(extractNumbers2('2025 год'));
 console.log(extractNumbers2('ECMAScript 2024'));
@@ -97,3 +65,94 @@ console.log(extractNumbers2(2025));
 console.log(extractNumbers2(-10));
 console.log(extractNumbers2(1.59));
 console.log('--ИЗВЛЕЧЕНИЕ ЦИФР ЗАВЕРШЕНО--');
+
+
+// 4. ФУНКЦИЯ ДЛЯ ПОИСКА НОМЕРА ПОДЪЕЗДА И ЭТАЖА ПО НОМЕРУ КВАРТИРЫ
+function calcEntranceNumAndFloorNum(floorsCount, appartmentsPerFloor, appartmentNumber) {
+  const appartmentsPerEntrance = floorsCount * appartmentsPerFloor;
+  const entranceNum = Math.ceil(appartmentNumber / appartmentsPerEntrance);
+  console.log('Номер подъзда ' + entranceNum);
+  const floorNum = Math.ceil((appartmentNumber - appartmentsPerEntrance * (entranceNum - 1)) / appartmentsPerFloor);
+  console.log('Номер этажа ' + floorNum);
+}
+calcEntranceNumAndFloorNum(5, 3, 45);
+console.log('--ПОИСК КВАРТИРЫ ЗАВЕРШЁН--');
+
+
+// 5. ФУНКЦИИ ДЛЯ РИСОВАНИЯ
+// Вариант 1. Прямоугольник
+const renderRectangle = (v, h) => {
+  let result = '';
+  for (let i = 1; i <= h; i++) {
+    for (let j = 1; j <= v; j++) {
+      result += '*';
+    }
+    result += '\n';
+  }
+  return result;
+};
+console.log(renderRectangle(6, 5));
+
+// Вариант 2. Прямоугольная рамка
+const renderRectangularFrame = (v, h) => {
+  let result = '';
+  for (let i = 1; i <= h; i++) {
+    if (i === 1 || i === h) {
+      for (let j = 1; j <= v; j++) {
+        result += '*';
+      }
+      result += '\n';
+    } else {
+      const spaces = ' '.repeat(v - 2);
+      result += '*' + spaces + '*' + '\n';
+    }
+  }
+  return result;
+};
+console.log(renderRectangularFrame(6, 5));
+
+// Вариант 3.1. Верхний левый прямоугольный треугольник (вариант А)
+const height1 = 5;
+for (let i = 1; i <= height1; i++) {
+  console.log('* '.repeat(i));
+}
+console.log('\n');
+
+// Вариант 3.2. Верхний левый прямоугольный треугольник (вариант Б)
+const height2 = 5;
+let counter1 = 0;
+for (let i = height2; i > 0; i--) {
+  counter1++;
+  console.log('* '.repeat(counter1) + ' '.repeat(i - 1));
+}
+console.log('\n');
+
+// Вариант 4. Верхний правый прямоугольный треуольник
+const height4 = 5;
+for (let i = 1; i <= height4; i++) {
+  console.log('  '.repeat((height4 - i)) + '* '.repeat(i));
+}
+console.log('\n');
+
+// Вариант 5. Нижний правый прямоугольный треуольник
+const height5 = 5;
+for (let i = height5; i >= 1; i--) {
+  console.log('  '.repeat(height5 - i) + '* '.repeat(i));
+}
+console.log('\n');
+
+// Вариант 6. Нижний левый прямоугольный треуольник
+const height6 = 5;
+for (let i = height6; i >= 1; i--) {
+  console.log('* '.repeat(i));
+}
+console.log('\n');
+
+// Вариант 4.2. Равнобедренный треугольник (собирался делать Верхний правый треугольник)
+const height7 = 5;
+let counter2 = 0;
+for (let i = height7; i > 0; i--) {
+  counter2++;
+  console.log(' '.repeat(i - 1) + '* '.repeat(counter2));
+}
+console.log('\n');
