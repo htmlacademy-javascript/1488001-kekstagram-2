78c84beb0a474bf3a8ca2aa146c43d52489a82f1
Михаил Гилодо <mgilodo@gmail.com>
2025-04-11 08:34:10 +0300

    изменение масштаба, эффекты для загружаемого пользователем изображения

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import { createPhotos } from './data.js';
 import { renderPhotos } from './render.js';
 import { openForm, closeForm } from './form.js';
+import { useEffects } from './effects.js';
 
 renderPhotos(createPhotos());
 
 openForm();
 closeForm();
+
+useEffects();
